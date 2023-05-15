b9b1073d6649ca4d0ab80d6ca1885e1ac45bafaa
Pedro Torres <45828743+phesgot@users.noreply.github.com>
2023-05-15 11:25:36 -0300

    lista de transações  e model

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,97 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _transactions = [
+      Transaction(
+        id: 't1',
+        title: 'Tênis',
+        value: 310.20,
+        date: DateTime.now(),
+      ),
+      Transaction(
+        id: 't2',
+        title: 'Conta de Luz',
+        value: 103.75,
+        date: DateTime.now(),
+      ),
+    ];
+
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xff581845),
-          title: const Text("Despesas Pessoais"),
-          centerTitle: true,
-          elevation: 0,
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            Card(
-              child: Text("Gráfico"),
-            ),
-            Card(
-              child: Text("Lista"),
-            )
-          ],
-        )
-      ),
+          appBar: AppBar(
+            backgroundColor: const Color(0xff581845),
+            title: const Text("Despesas Pessoais"),
+            centerTitle: true,
+            elevation: 0,
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Card(
+                child: Text("Gráfico"),
+              ),
+              Column(
+                children: _transactions.map((tr) {
+                  return Card(
+                      elevation: 4,
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 15,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                              color: const Color(0xff900c3f),
+                              width: 2,
+                            )),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              "R\$ ${tr.value.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff900c3f),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  tr.title,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat('d MMM y').format(tr.date),
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ));
+                }).toList(),
+              ),
+            ],
+          )),
     );
   }
 }
-
 
 // #900c3f
 // #581845
